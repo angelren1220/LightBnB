58694b7324f6b0c5339fdb09fdf2992f392135dc
Linuxe <angelren1220@gmail.com>
2023-04-05 16:54:05 -0400

    add 1-1 quries

---
+++ 1_1_queries/03_property_listings_by_city.sql
@@ -0,0 +1,11 @@
+SELECT
+properties.id AS id, title, cost_per_night,
+AVG(rating) AS average_rating
+FROM properties
+LEFT JOIN property_reviews
+ON properties.id = property_id
+WHERE city Like '%ancouver'
+GROUP BY properties.id
+HAVING AVG(rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
---
+++ 1_1_queries/05_all_my_reservations.sql
@@ -0,0 +1,11 @@
+SELECT
+reservations.id AS id, title, cost_per_night, start_date,
+AVG(rating) AS average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+JOIN users ON reservations.guest_id = users.id
+WHERE reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
